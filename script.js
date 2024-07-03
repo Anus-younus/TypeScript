efb039084eb38f4ff41556590e8b75b30339c179
Anus Younus <AnusYounus254@gmail.com>
2024-07-03 22:15:13 +0500

    added my all changes

---
+++ script.js
@@ -0,0 +1,90 @@
+"use strict";
+// let str: string = "Hello"
+// let array: string[] = ["Anus", "Owasi"]
+// array.push("Anus")
+// console.log(str, array);
+//constant array
+// let array: [string, boolean, number] = ["Hello", true, 2]
+// enum User {
+//     name = "Anus",
+//     email = "anus@gmail.com",
+//     password = "anus"
+// }
+// console.log(User.password);
+// function getUser(): string{
+//     return "Anus"
+// }
+// console.log(getUser());
+// function getUndo(): void{
+// }
+// console.log(getUndo());
+// let userName: string | number = "Anus"
+// userName = 22
+// let array: (string | number | boolean)[] = ["Anus", 2322, true]
+// array.push(false)
+// type User = {
+//     userNmae: string,
+//     rolNumber: number,
+//     shirtNum?: string | number
+//     email: string
+// }
+// let user: User = {
+//     userNmae: "Anus",
+//     rolNumber: 23304,
+//     email: "anusyounus@gmail.com"
+// }
+// user.shirtNum = "234"
+// console.log(user);
+// let users = [
+//     {
+//         userName: "Anus",
+//         email: "anusyounus@gmail.com",
+//         password: "anus1223",
+//         phone: "03242373254"   
+//     },
+//     {
+//         userName: "Owais",
+//         email: "anusyounus@gmail.com",
+//         password: "anus1223",
+//         phone: "03242373254"   
+//     },
+//     {
+//         userName: "Hassan",
+//         email: "anusyounus@gmail.com",
+//         password: "anus1223",
+//         phone: "03242373254"   
+//     },
+//     {
+//         userName: "Younus",
+//         email: "anusyounus@gmail.com",
+//         password: "anus1223",
+//         phone: "03242373254"   
+//     },
+//     {
+//         userName: "Manan",
+//         email: "anusyounus@gmail.com",
+//         password: "anus1223",
+//         phone: "03242373254"   
+//     }
+// ]
+// users.forEach(user => {
+//     console.log(user);
+// })
+// type Car = {
+//     name: string,
+//     model: number,
+//     varient: string,
+//     isAutomatic: boolean,
+//     features: string[],
+//     launchedYear?: number,
+//     calculateAvg?: (fuelQty: number, distance: number) => void
+// }
+// let car: Car = {
+//     name: 'Mehran',
+//     model: 2009,
+//     varient: 'VXR',
+//     isAutomatic: false,
+//     features: ["Air Bags", "Air Conditioned"],
+//     calculateAvg: (fuelQty, distance) => {
+//     }
+// }
